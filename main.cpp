736f275d9bdf90791c768fff784dee14b7b9686a
Loïc Dessaules <loic.dessaules@gmail.com>
2019-06-05 16:06:40 +0200

    Ajout des fichiers de base et commentaires des containeurs a utiliser et pk

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+/*
+ * TODO list
+ * - CONTAINEURS : - Une Queue qui stock les noeuds à traité (on ajoute au fur et a mesure les voisins)
+ *                 - Un Array qui stock ce qui a été visité, par qui => v[10] = 11 le noeud 10 a été visité par 11
+ *                 - Un Map qui stock la clé (string de la séquence) avec une valeur qui est le noeud associé à la clé
+ *                 => par exemple on a un voisin qui est "1324058976" si cette valeur existe dans notre Map, et son noeud
+ *                    associé (valeur de la clé "1324058976") est 10, on sait qu'il a déjà été visité donc on va pas le revisiter
+ *
+ * - Parcours en largeur qui prend en param
+ */
+
+using namespace std;
+
+int main() {
+
+
+
+    return EXIT_SUCCESS;
+}
